19b738f9d8019c702d27bc8f376d1b168c8d6aa6
Aashu Gupta <aashugupta932@gmail.com>
2026-05-15 23:30:40 +0530

    Enhance DOM.js with window object and DOM explanations
    Added comments explaining the DOM and window object.

---
+++ basics/javascript/DOM.js
@@ -0,0 +1,22 @@
+//DOM {Document Object Model}
+//window object
+//the window object represent an open window in a browser. it is browser's object (not  A  javascripts & is automatically created by browser).
+//it is a global object with lots of properties & methods.
+//window
+// console.log("hello");
+// window.console.log("hello 2");  //console.log know about window and it is a part of  window like alert , console.log, prompt
+//imp for interview
+//whta is a DOM=[when a web page is loaded , the browser creates a document object model (dom) of th page]
+// it is a tree like structure
+//                  window  //nodes
+//                 document   //nodes
+//                   html
+//    head                            body
+//meta meta title link             div script
+//                                img h1 p div
+// console.log (); used for print 
+// console.log(document);
+// console.dir(window.document);
+console.dir(document.body);
+console.log(document.body);
+
